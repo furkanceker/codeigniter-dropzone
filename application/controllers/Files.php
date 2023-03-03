caf7527dd467f2773a4fe8f4a60c3482627d268c
Furkan Çeker <ceker0901@gmail.com>
2023-03-03 13:43:31 +0300

    firts commit

---
+++ application/controllers/Files.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Files extends CI_Controller {
+
+    public function index(){
+        $this->load->view('upload_view');
+    }
+
+}
---
+++ application/views/upload_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Çoklu Dosya Yükleme</title>
+    <link rel="stylesheet" href="<?= base_url('assets/dropzone.scss') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/custom.css') ?>">
+</head>
+<div class="container">
+    <h1 class="text-center">Dropzone İle Çoklu Dosya Yükleme</h1>
+
+</div>
+<body>
+    <script src="<?= base_url('assets/dropzone.js') ?>"></script>
+    <script src="<?= base_url('assets/jquery-3.6.3.min.js') ?>"></script>
+    <script src="<?= base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/custom.js') ?>"></script>
+    
+</body>
+</html>
